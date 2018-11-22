351ed95e4c50e0606a3497d6769ed21a6353d8be
CinthiaVilcachagua <cv.ccori@gmail.com>
2018-11-22 13:18:43 -0500

    Giving interaction to my html with DOM

---
+++ src/index.js
@@ -0,0 +1,61 @@
+//Creamos la variable y llamamos a los valores del form2,
+  //llamamos al boton de Siguiente
+  const btnId = document.getElementById('boton-id');
+  //creamos la variable y llamamos a los valores del form 2
+  const id = document.getElementById('respuesta-id');
+ //llamamos a la variable y respuesta de password
+  const password = document.getElementById('respuesta-password');
+
+  const btnNext = document.getElementById('boton-siguiente');
+
+  const btnReturn = document.getElementById('boton-volver');
+
+  document.getElementById('form2').style.display='none';
+  document.getElementById('form3').style.display='none';
+
+  //funcion captar Letras
+  function captarLetras(word,n){
+    let resultWord = '';
+    for(let i = 0; i < n; i++){
+      resultWord += word[i];
+    }
+    return resultWord;
+  }
+
+  btnNext.addEventListener('click',function(){
+      document.getElementById('form1').style.display='none'; //desaparece el form1
+      document.getElementById('form2').style.display='block'; //aperece el form2
+  })
+
+
+
+
+    btnId.addEventListener('click', function (){
+      document.getElementById('form2').style.display='none'; //desaparece el form2
+      document.getElementById('form3').style.display='block'; //aparece el form3
+      const offset = parseInt(document.getElementById('offset').value);
+
+      let string1= document.getElementById('id-1').value;
+      let string2= document.getElementById('id-2').value;
+      let string3= document.getElementById('id-3').value;
+
+      string1 = captarLetras(string1,2);
+      string2 = captarLetras(string2,2);
+      string3 = captarLetras(string3,2);
+
+
+      id.value = '2018' + cipher.encode(string1,offset) + cipher.encode(string2,offset) + cipher.encode(string3,offset); //si colocamos el parseInt en la funcion
+      id.disabled ='disabled';
+
+
+
+      password.value = cipher.decode(string3,offset) + cipher.decode(string2,offset) + cipher.decode(string1,offset); // se coloca value, porque yo inserto el valor a un input
+      //seia getelemnetbyid solo si yo inserto el texto a un p o h1, etc.
+      password.disabled = 'disabled'; //para que nadie pueda tocarlo
+  })
+
+
+  btnReturn.addEventListener('click',function(){
+      document.getElementById('form1').style.display = 'block';
+      document.getElementById('form3').style.display = 'none';
+  })
