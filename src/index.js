40ebcb26fb38f0dc628d382d8a27b4897cf52ed2
CinthiaVilcachagua <cv.ccori@gmail.com>
2018-12-05 10:56:18 -0500

    Llenado de campos d emanera obligatoria-Alerta

--- src/index.js
+++ src/index.js
@@ -30,30 +30,35 @@
   })
 
   //
-    btnNext.addEventListener('click', () => {
+  btnNext.addEventListener('click', () => {
+    const offset = parseInt(document.getElementById('offset').value);
+    let string1= document.getElementById('id-1').value;
+    let string2= document.getElementById('id-2').value;
+    let string3= document.getElementById('id-3').value;
+
+    if(offset==="" || string1=== "" || string2=== "" || string3=== ""  ){
+        alert("llenar los campos para poder generar tu código");
+    } else {
       document.getElementById('form_screen').setAttribute('class','ocultar'); //desaparece el form2
       document.getElementById('result_screen').setAttribute('class','visible'); //aparece el form3
       document.getElementById('cuerpo').setAttribute('class','fondo3');
-
-      const offset = parseInt(document.getElementById('offset').value);
-
-      let string1= document.getElementById('id-1').value;
-      let string2= document.getElementById('id-2').value;
-      let string3= document.getElementById('id-3').value;
 
       string1 = captarLetras(string1,2);
       string2 = captarLetras(string2,2);
       string3 = captarLetras(string3,2);
 
 
-      id.value = '2018' + cipher.encode(offset,string1) + cipher.encode(offset,string2) + cipher.encode(offset,string3); //si colocamos el parseInt en la funcion
-      id.disabled ='disabled';
+        id.value = '2018' + cipher.encode(offset,string1) + cipher.encode(offset,string2) + cipher.encode(offset,string3); //si colocamos el parseInt en la funcion
+        id.disabled ='disabled';
 
 
 
-      password.value = cipher.decode(offset,string3) + cipher.decode(offset,string2) + cipher.decode(offset,string1); // se coloca value, porque yo inserto el valor a un input
-      //seia getelemnetbyid solo si yo inserto el texto a un p o h1, etc.
-      password.disabled = 'disabled'; //para que nadie pueda tocarlo
+        password.value = cipher.decode(offset,string3) + cipher.decode(offset,string2) + cipher.decode(offset,string1); // se coloca value, porque yo inserto el valor a un input
+        //seia getelemnetbyid solo si yo inserto el texto a un p o h1, etc.
+        password.disabled = 'disabled'; //para que nadie pueda tocarlo
+      }
+
+
   })
 
   btnReturn.addEventListener('click',() => {
